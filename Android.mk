13b6625f729fe41d6dbc9abd00ff7b032ebf2659
nazephyrus <naden.irsyad01@gmail.com>
2026-03-14 15:38:42 +0700

    push tree to public

---
+++ Android.mk
@@ -0,0 +1,22 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),LH8n)
+
+$(call add-radio-file-sha1-checked,radio/dpm.img,e0622e1eb11ee2d306b5fd9fb6748d768fa62ac8)
+$(call add-radio-file-sha1-checked,radio/gz.img,bb284186c25aeddfa6502451ff61c49a98025f1d)
+$(call add-radio-file-sha1-checked,radio/lk.img,0899f26bae13e4ea33e023edf3002e534f7d7412)
+$(call add-radio-file-sha1-checked,radio/logo.img,2ae19ac76fa79a252adf7106976c19cb8df80bec)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,a7a945a6d5f7885de00b3aa649c2c20f716f7467)
+$(call add-radio-file-sha1-checked,radio/md1img.img,ecb26a785a17a800f4edf593bdd2a99506860f9f)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,20104f04bc03560410a1fb592dd25c661008976a)
+$(call add-radio-file-sha1-checked,radio/scp.img,11f7edb30e190146a6ebb69b18c7c543018a1d01)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,d54cbfc13fe8dee787f8b3f2e01824d9e52722bf)
+$(call add-radio-file-sha1-checked,radio/sspm.img,2e398bb0621fe89cb7bea078989c5f2efccb0f41)
+$(call add-radio-file-sha1-checked,radio/tee.img,87d0027be628ce60bbd59a7e30ebff601a623196)
+$(call add-radio-file-sha1-checked,radio/tkv.img,45fc1bb0a7036ed09b5ce7edeb503b5b67cd93a2)
+
+endif
