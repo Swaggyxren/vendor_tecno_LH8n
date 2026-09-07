6e155d80e2457d5b4fee577670ab9101925e763e
Swaggyxren <masambo67@gmail.com>
2026-09-07 22:03:21 +0800

    LH8n: ims: Bundle mediatek-ims-base stubs and add IMS props

--- ims/ims.mk
+++ ims/ims.mk
@@ -27,3 +27,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     mtk-ims \
     mtk-ims-telephony
+
+# IMS Properties
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1 \
+    persist.vendor.vilte_support=0 \
+    persist.vendor.ims_support=1 \
+    persist.vendor.volte_support=1
+
