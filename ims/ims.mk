2eb4e4c36fe8290c7df1e2b86e00fdde139b4aab
Swaggyxren <masambo67@gmail.com>
2026-09-07 22:05:17 +0800

    LH8n: ims: Disable viwifi_support to prevent vtservice crash

--- ims/ims.mk
+++ ims/ims.mk
@@ -34,6 +34,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.dbg.vt_avail_ovr=1 \
     persist.dbg.wfc_avail_ovr=1 \
     persist.vendor.vilte_support=0 \
+    persist.vendor.viwifi_support=0 \
     persist.vendor.ims_support=1 \
     persist.vendor.volte_support=1
 
